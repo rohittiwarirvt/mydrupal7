0a15bb4e6428e96b01adf75cbece5a61e073bbad
rohittiwarirvt <rohittiwarirvt@gmail.com>
2017-04-23 18:59:47 +0530

    Added scroll spy feature for about us page

--- sites/all/themes/prohaktiv/template.php
+++ sites/all/themes/prohaktiv/template.php
@@ -13,3 +13,11 @@ function prohaktiv_preprocess_page(&$vars) {
     drupal_add_js(drupal_get_path('theme', 'prohaktiv') . '/js/jquery.bxslider.js');
   }
 }
+
+function prohaktiv_preprocess_html(&$vars) {
+  $path = drupal_get_path_alias();
+  $aliases = explode('/', $path);
+  foreach($aliases as $alias) {
+    $vars['classes_array'][] = drupal_clean_css_identifier($alias);
+  }
+}
