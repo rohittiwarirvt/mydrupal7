4995e67bffb8f262f88655fcdf7ac742f64d752e
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-08-26 11:31:43 +0530

    Added some design css and fonts

--- thefactoryshop/sites/all/themes/custom/thfactoryshop/template.php
+++ thefactoryshop/sites/all/themes/custom/thfactoryshop/template.php
@@ -3,3 +3,27 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+
+function thefactoryshop_preprocess_views_view_unformatted(&$vars) {
+  // $view = $vars['view'];
+  // $rows = $vars['rows'];
+  // switch ($view->name) {
+  //   case 'home_page_catalog':
+  //     foreach ($rows as  $id => $row) {
+  //       //$vars['classes_array'][$key][]='col-md-4';
+  //       if ($row_class = $view->style_plugin->getRowClass($id)) {
+  //         dsm($row_class);
+  //         $vars['rows'][$id]['attributes']->addClass($row_class);
+  //       }
+  //     }
+  //     break;
+
+  //   default:
+  //     # code...
+  //     break;
+  // }
+  // //dsm($vars['classes']);
+  // dsm($vars['classes_array'][1]);
+}
+
